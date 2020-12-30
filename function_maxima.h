c6a091f6f114f8dcc541c6dbb99d86cc80fc3962
Michał Traczyk <mt418401@students.mimuw.edu.pl>
2020-12-30 16:09:06 +0100

    Some stuff added

--- function_maxima.h
+++ function_maxima.h
@@ -1,19 +1,8 @@
 #ifndef FUNCTION_MAXIMA_H
 #define FUNCTION_MAXIMA_H
 
-namespace {
-    class GuardPointTypeConstruction {
-    public:
-        GuardPointTypeConstruction();
-
-        ~GuardPointTypeConstruction() noexcept;
-
-        void done() noexcept;
-
-    private:
-        bool reverse;
-    };
-}
+#include <set>
+#include <memory>
 
 template<typename A, typename V>
 class FunctionMaxima {
@@ -34,9 +23,9 @@ private:
 template<typename A, typename V>
 class FunctionMaxima<A, V>::PointType {
 public:
-    PointType(const PointType &other) noexcept;
+    PointType(const PointType &other) noexcept = default;
 
-    PointType &operator=(const PointType &other);
+    PointType &operator=(PointType other) noexcept;
 
 private:
     PointType(const A &arg, const V &val);
@@ -51,43 +40,71 @@ private:
     std::shared_ptr<V> value;
 };
 
-GuardPointTypeConstruction::GuardPointTypeConstruction() {
+namespace {
+    template<typename A>
+    class Guard {
+    public:
+        Guard(std::shared_ptr<A> *data);
 
-}
+        ~Guard() noexcept;
 
-GuardPointTypeConstruction::~GuardPointTypeConstruction() noexcept {
+        void done() noexcept;
 
-}
+    private:
+        std::shared_ptr<A> *m_data;
+        bool reverse;
+    };
 
-void GuardPointTypeConstruction::done() noexcept {
+    template<typename A>
+    Guard<A>::Guard(std::shared_ptr<A> *data) : reverse(true), m_data(data) {}
 
-}
+    template<typename A>
+    Guard<A>::~Guard() noexcept {
+        if (reverse) {
+            m_data->reset();
+        }
+    }
 
-template<typename A, typename V>
-FunctionMaxima<A, V>::PointType::PointType(const FunctionMaxima<A, V>::PointType &other) noexcept {
-
+    template<typename A>
+    void Guard<A>::done() noexcept {
+        reverse = false;
+    }
 }
 
 template<typename A, typename V>
 typename FunctionMaxima<A, V>::PointType &
-FunctionMaxima<A, V>::PointType::operator=(const FunctionMaxima<A, V>::PointType &other) {
+FunctionMaxima<A, V>::PointType::operator=(FunctionMaxima<A, V>::PointType other) noexcept {
+    argument.swap(other.arg());
+    value.swap(other.val());
 
+    return *this;
 }
 
 template<typename A, typename V>
 FunctionMaxima<A, V>::PointType::PointType(const A &arg, const V &val) {
-
+    try {
+        argument = std::make_shared<A>(new A(arg));
+        Guard<A> point_construction_guard = Guard<A>(&argument);
+        value = std::make_shared<A>(new V(val));
+        point_construction_guard.done();
+    } catch (...) {
+        throw;
+    }
 }
 
 template<typename A, typename V>
 A const &FunctionMaxima<A, V>::PointType::arg() const {
-
+    return *argument.get();
 }
 
 template<typename A, typename V>
 V const &FunctionMaxima<A, V>::PointType::val() const {
-
+    return *value.get();
 }
 
+template<typename A, typename V>
+typename FunctionMaxima<A, V>::PointType create_point(const A &arg, const V &val) {
+    return FunctionMaxima<A, V>::PointType::PointType(arg, val);
+}
 
 #endif // FUNCTION_MAXIMA_H
